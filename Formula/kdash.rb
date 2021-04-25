44c9e274cf0787718a538cefe817c7dc812206d5
deepu105 <d4udts@gmail.com>
2021-04-25 23:15:08 +0200

    add CI

--- Formula/kdash.rb
+++ Formula/kdash.rb
@@ -4,10 +4,10 @@ class Kdash < Formula
   desc "A fast and simple dashboard for Kubernetes written in Rust"
   homepage "https://github.com/kdash-rs/kdash"
   if OS.mac?
-    url "https://github.com/kdash-rs/kdash/releases/latest/download/kdash-macos.tar.gz"
+    url "https://github.com/kdash-rs/kdash/releases/download/v0.0.3/kdash-macos.tar.gz"
     sha256 "f9d4107a027514e136b2e74474bca76af96987d15879dcbaabbabec4aee39395"
   else
-    url "https://github.com/kdash-rs/kdash/releases/latest/download/kdash-linux.tar.gz"
+    url "https://github.com/kdash-rs/kdash/releases/download/v0.0.3/kdash-linux.tar.gz"
     sha256 "4774c6540be48d020faa06b6172a46cceb9e033663566e0ab174e8475d5ac0f3"
   end
   version "0.0.3"
