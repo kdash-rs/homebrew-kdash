ffa83d4cf71a5b7ce24881b76803c9a21c3a6d72
deepu105 <d4udts@gmail.com>
2021-05-17 14:05:52 +0000

    Update formula for KDash release v0.0.10

--- Formula/kdash.rb
+++ Formula/kdash.rb
@@ -6,10 +6,10 @@ class Kdash < Formula
   homepage "https://github.com/kdash-rs/kdash"
   if OS.mac?
     url "https://github.com/kdash-rs/kdash/releases/download/v0.0.10/kdash-macos.tar.gz"
-    sha256 "12358"
+    sha256 "005515c395d4c4e2610cbcfeb6786d671626988766a7f16dc5dd61c046c38963"
   else
     url "https://github.com/kdash-rs/kdash/releases/download/v0.0.10/kdash-linux-musl.tar.gz"
-    sha256 "12345"
+    sha256 "7e8d020ec2d871beb067cd3a25f207a03d03cacd2465740c620374bccb7ef7c0"
   end
   version "v0.0.10"
   license "MIT"
