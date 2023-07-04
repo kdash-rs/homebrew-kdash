3208ed996c30b8d9b4b07e1aaf494ac448d71f47
deepu105 <d4udts@gmail.com>
2023-07-04 20:49:08 +0000

    Update formula for KDash release v0.4.0

--- Formula/kdash.rb
+++ Formula/kdash.rb
@@ -5,13 +5,13 @@ class Kdash < Formula
   desc "A fast and simple dashboard for Kubernetes written in Rust"
   homepage "https://github.com/kdash-rs/kdash"
   if OS.mac?
-    url "https://github.com/kdash-rs/kdash/releases/download/v0.3.7/kdash-macos.tar.gz"
-    sha256 "f650950849a317d545ae9ef6bf46f552f58d4d231b47599f337fb9ca410479e5"
+    url "https://github.com/kdash-rs/kdash/releases/download/v0.4.0/kdash-macos.tar.gz"
+    sha256 "6e7e4c00cec0a14fc25dc525efc282938971f547f72164ecec2ff3bef30ebb10"
   else
-    url "https://github.com/kdash-rs/kdash/releases/download/v0.3.7/kdash-linux-musl.tar.gz"
-    sha256 "c5d7a91bcc3c6d7ad0c9931f220d6726c76b5947574b37baf0cdd33301a6aaee"
+    url "https://github.com/kdash-rs/kdash/releases/download/v0.4.0/kdash-linux-musl.tar.gz"
+    sha256 "1c6ad268a688e127707ee504e64c163b9d3e372ce9d88678c39cdaa87093fd77"
   end
-  version "v0.3.7"
+  version "v0.4.0"
   license "MIT"
 
   def install
